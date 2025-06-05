f525082fb1bb30764b09b4d2219c4a0681356875
santiago123x <santiago_123x@hotmail.com>
2025-06-04 19:56:28 -0500

    Card Section

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { CardProps } from "@/types/interfaces";
+import styles from "./Card.module.css";
+
+const Card: React.FC<CardProps> = ({
+  description,
+  image,
+  price,
+  bought,
+  review,
+}) => {
+  const money_symbol: string = "US$";
+
+  const renderStars = () => {
+    return "★".repeat(Math.floor(review)) + "☆".repeat(5 - Math.floor(review));
+  };
+
+  return (
+    <div className={styles.card_container}>
+      <div className={styles.container_image}>
+        <img
+          alt={`${description.slice(0, 10)}${
+            description.length > 10 ? "..." : ""
+          }`}
+          src={image}
+          loading="lazy"
+        />
+      </div>
+
+      <div className={styles.container_description}>
+        <p>{description}</p>
+      </div>
+
+      <div className={styles.container_reviews}>
+        <div className={styles.reviews_stars}>{renderStars()}</div>
+        <div className={styles.reviews_bought}>{bought} vendidos</div>
+      </div>
+
+      <div className={styles.container_price}>
+        <span>{money_symbol}</span>
+        <span>{price.toFixed(2)}</span>
+      </div>
+
+      <div className={styles.container_buttons}>
+        <button className={styles.car_button}>Carrito</button>
+        <button className={styles.buy_button}>Comprar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/types/interfaces.ts
@@ -0,0 +1,13 @@
+interface CardProps {
+    description: string;
+    image: string;
+    price: number;
+    bought: number;
+    review: number;
+    category?: string;
+  }
+
+
+export type {
+    CardProps
+}

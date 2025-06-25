dd4f4e454f9333aa29da75a45cd8ba09bf02506e
JohansonCda <jcv9806@hotmail.com>
2025-06-25 17:42:53 -0500

    Move header to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 import React from "react";
 import CardSection from "@/components/cardSection/CardSection";
 import BannerCarrousel from '@/components/bannerCarrousel/BannerCarrousel';
-import HeaderSection from '@/components/headerSection/HeaderSection';
+
 
 const Home = () => {
 
   return (
     <div>
-      <HeaderSection />
+      
       <CardSection />
       <BannerCarrousel />
     </div>
